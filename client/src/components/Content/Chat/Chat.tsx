059ffab6b294e787702fae8e73acc9015f69c607
ValentinKoruev <doopster123@gmail.com>
2025-08-04 23:20:40 +0300

    styling: messages to be always detailed if previous message is older than 30 minutes

--- client/src/components/Content/Chat/Chat.tsx
+++ client/src/components/Content/Chat/Chat.tsx
@@ -1,5 +1,7 @@
 import { FC } from 'react';
+import { differenceInMinutes } from 'date-fns';
 import useChat from './hooks/useChat';
+import { MessageDto } from '@shared/types/dto/Message';
 import Icon from '@shared/components/Icon';
 import Message from './Message';
 import ChatBottomBar from '@components/Content/Chat/ChatBottomBar';
@@ -10,15 +12,24 @@ export type ChatProps = {};
 const Chat: FC<ChatProps> = () => {
   const { activeChannelId, messages, title } = useChat();
 
+  const isDetailed = (message: MessageDto, prevMessage: MessageDto) => {
+    if (!prevMessage) return true;
+
+    if (message.senderId != prevMessage.senderId) return true;
+
+    const diffInMinutes = Math.abs(differenceInMinutes(message.date, prevMessage.date));
+
+    return diffInMinutes > 30;
+  };
+
   const renderMessages = () => {
     if (!activeChannelId) return;
 
     return messages.map((mes, index, array) => {
-      const isDetailed = array[index - 1]?.senderId !== mes.senderId;
       return (
         <Message
           key={`message-${index}`}
-          detailed={isDetailed}
+          detailed={isDetailed(mes, array[index - 1])}
           username={mes.senderName}
           date={mes.date}
           content={mes.content}
