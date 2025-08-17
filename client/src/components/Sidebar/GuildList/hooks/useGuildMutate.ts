0f3763637b61753415f0b55c3310fd33120a0019
ValentinKoruev <doopster123@gmail.com>
2025-08-17 21:19:27 +0300

    fix: guild tab indicator not working when guild has no channel

--- client/src/components/Sidebar/GuildList/hooks/useGuildMutate.ts
+++ client/src/components/Sidebar/GuildList/hooks/useGuildMutate.ts
@@ -17,31 +17,46 @@ const useMutate = ({ setSidebar }: { setSidebar: any }) => {
     onSuccess: async (guild) => {
       if (!guild) return;
 
+      dispatch(setTabGuild(guild.guildId));
       setSidebar({ type: 'guild', currentGuild: guild });
 
-      if (guild.channels) {
-        const channelResponse = await apiQueries.channelQueries.getChannel(
-          formatGuildChannel(guild.guildId, guild.channels[0].id),
-        );
-
-        // TODO: Change it to last visited channel when it is supported
-        const initialGuildChannel = channelResponse.data;
-
-        // TODO: expand this error handler
-        if (!initialGuildChannel) {
-          console.assert('Error: No channel found');
-          return;
-        }
-
-        dispatch(setTabGuild(guild.guildId));
-        dispatch(setActiveChannel({ type: 'guild', channelId: initialGuildChannel.id, guildId: guild.guildId }));
+      if (!guild.channels || guild.channels.length <= 0) {
         dispatch(
           setChatChannel({
-            channelName: initialGuildChannel.name,
-            messages: initialGuildChannel.messages,
+            channelName: null,
+            messages: [],
           }),
         );
+        return;
       }
+
+      //! NOTE: this triggers error when no channel is found, handle in onError in the future
+      const channelResponse = await apiQueries.channelQueries.getChannel(
+        formatGuildChannel(guild.guildId, guild.channels[0].id),
+      );
+
+      // TODO: Change it to last visited channel when it is supported
+      const initialGuildChannel = channelResponse.data;
+
+      // TODO: expand this error handler
+      if (!initialGuildChannel) {
+        console.error('Error: Channel not found');
+        dispatch(
+          setChatChannel({
+            channelName: null,
+            messages: [],
+          }),
+        );
+        return;
+      }
+
+      dispatch(setActiveChannel({ type: 'guild', channelId: initialGuildChannel.id, guildId: guild.guildId }));
+      dispatch(
+        setChatChannel({
+          channelName: initialGuildChannel.name,
+          messages: initialGuildChannel.messages,
+        }),
+      );
     },
   });
 
