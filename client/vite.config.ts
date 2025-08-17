fcd770e9db94f478d0672a424f05f86c4a4437cf
ValentinKoruev <doopster123@gmail.com>
2025-08-17 17:32:51 +0300

    build: vite styles alias

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,8 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@styles': path.resolve(__dirname, './src/styles'),
+    },
+  },
   plugins: [react(), tsconfigPaths()],
-})
+});
