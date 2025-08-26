eaa5b2dc42113a045d6516bfd18962b78ab7c57a
ValentinKoruev <doopster123@gmail.com>
2025-08-26 22:47:02 +0300

    build: server to get host from env

--- server/src/main.ts
+++ server/src/main.ts
@@ -9,6 +9,6 @@ async function bootstrap() {
     credentials: true,
   });
   app.use(cookieParser());
-  await app.listen(process.env.PORT ?? 4000);
+  await app.listen(process.env.PORT ?? 4000, process.env.HOST ?? 'localhost');
 }
 bootstrap();
